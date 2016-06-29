175bd2a4e783dda7ae9c706718703ddacad6f75d
crazonaut <iogata21@punahou.edu>
2016-06-28 14:28:18 -1000

    Added stuff
    I added stuff to the list_app like images instead of colors, and i made
    it change the background instead of saying it in the log

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-var colors = [
-	"red", 
-	"orange", 
-	"yellow", 
-	"green",
-	"blue",
-	"indigo",
-	"violet",
+var backrounds = [
+	"url('../assets/wat.gif')", 
+	"url('../assets/banana.gif')", 
+	"url('../assets/veg.gif')", 
+	"url('../assets/keyboardcat.gif')",
+	"url('../assets/cookie.gif')",
+	"url('../assets/nyan.gif')",
+	"url('../assets/lel.gif')",
+	"url('../assets/mickey.gif')",
+	"url('../assets/type.gif')",
+	"url('../assets/dtrump.gif')",
+	"url('../assets/dravenyay.gif')",
+	"url('../assets/freddy.gif')",
+	"url('../assets/MURO.gif')"
 	];
  
 var count = 0;
 
 var cycle = function(array) {
-	console.log(colors[count]);
+	// console.log(colors[count]);
 	if (count == array.length-1) {
 		count = 0;
 	} else {
@@ -21,5 +27,6 @@ var cycle = function(array) {
 };
 
 var display = function() {
-	cycle(colors);
+	document.body.style.backgroundImage = backrounds[count];
+	cycle(backrounds);
 };
